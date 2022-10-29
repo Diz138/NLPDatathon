7df6428e936a3f8e5d41fb9e7e21b93d62bea492
Diz138 <mattdzwil@gmail.com>
2022-10-29 13:37:27 -0400

    nlp update

--- nlptrial.py
+++ nlptrial.py
@@ -2,7 +2,7 @@ import pandas as pd
 import nltk
 from nltk.tokenize import word_tokenize
 #from nltk.tokenize import sent_tokenize
-#from nltk.probability import FreqDist
+from nltk.probability import FreqDist
 #nltk.download('stopwords')
 #nltk.download('punkt')
 #nltk.download('wordnet')
@@ -22,13 +22,22 @@ def lemmatize_text(text):
 def top100(df):
     # Set up
     df["word_tok"] = df.methods.apply(lambda L: re.sub(re.compile('<.*?>'), '', L))
-    df.word_tok = df.word_tok.apply(lambda L: re.sub('[^A-Za-z0-9]+', ' ', L))
+    df.word_tok = df.word_tok.apply(lambda L: re.sub('[^A-Za-z]+', ' ', L))
     df.word_tok = df.word_tok.apply(lambda L: L.lower())
     df.word_tok = df.word_tok.apply(lemmatize_text)
-    return df
+    freq_test = []
+    for i in range(df.shape[0]):
+        freq_test = freq_test + df.word_tok[i]
+    filtered_words = []
+    for word in freq_test:
+        filtered_words.append(word)
+    fdist = FreqDist(filtered_words)
+
+    return fdist.most_common(100), df, filtered_words
 def main():
     df = pd.read_csv('mockData.csv')
     return top100(df)
 
 if __name__ == "__main__":
-    main().to_csv("mockDataLem.csv")
+    mostCommon, _, _ =main()
+    print(mostCommon)
