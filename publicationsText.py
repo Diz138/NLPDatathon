b40b2603d20cd982a40252e6ae5b7246e1e18051
Diz138 <mattdzwil@gmail.com>
2022-10-30 13:38:21 -0400

    json long cai

--- publicationsText.py
+++ publicationsText.py
@@ -31,20 +31,22 @@ if __name__ == '__main__':
         publications = json.load(f)
     i = 0
     for author in publications:
-        out_json[author] = {}
-        for pub in publications[author]:
-            if pub['bib']['title'] != 'Orientations of side chains and adsorbed liquid crystal molecules on a rubbed polyimide surface studied by optical second harmonic generation':
-                print("Getting text for: " + pub['bib']['title'])
-                url = pub['pub_url']
-                driver.get(url)
-                html = driver.page_source
-                soup = BeautifulSoup(html)
-                out_json[author][pub['bib']['title']] = soup.get_text()
-                # print(out_json[author][pub['bib']['title']])
-                if i % 100 == 0:
-                    with open(f'publications_text_{i}.json', 'w') as f:
-                        json.dump(out_json, f, indent=4)
-            i += 1
+        if author != 'xiaowei zhuang' and 'long cai':
+            out_json[author] = {}
+            for pub in publications[author]:
+
+                if pub['bib']['title'] != 'Turning single cells into microarrays by super-resolution barcoding':
+                    print("Getting text for: " + pub['bib']['title'])
+                    url = pub['pub_url']
+                    driver.get(url)
+                    html = driver.page_source
+                    soup = BeautifulSoup(html)
+                    out_json[author][pub['bib']['title']] = soup.get_text()
+                    # print(out_json[author][pub['bib']['title']])
+                    if i % 30 == 0:
+                        with open(f'publications_text_{i}.json', 'w') as f:
+                            json.dump(out_json, f, indent=4)
+                i += 1
 
     with open(f'publications_text_{i}.json', 'w') as f:
         json.dump(out_json, f, indent=4)
